108202cf59b22b22663953379caa1c145ee2a7ba
Xander Jones <xander@bugsnag.com>
2020-05-06 20:24:41 +0100

    Convert to new format

--- upper.py
+++ upper.py
@@ -1,8 +1,22 @@
+'''
+    Script:      my-tools/upper.py
+    Description: Convert any following arguments to upper case
+    Author:      Xander Jones (xander@xljones.com)
+    Web:         xljones.com
+    Date:        06 May 2020
+'''
+
+import argparse
+import os
 import sys
 
-str = ""
-for index, arg in enumerate(sys.argv):
-    if index > 0:
-        str += "{0} ".format(arg.upper())
-        
-print(str.upper())
+_VERSION = "1.0.0"
+_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+if (__name__ == "__main__"):
+    str = ""
+    for index, arg in enumerate(sys.argv):
+        if index > 0:
+            str += "{0} ".format(arg.upper())
+            
+    print(str.upper())
