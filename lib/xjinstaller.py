efc0fafde786d54c769f1bf366f7338c6e9b67d0
Xander Jones <xander@bugsnag.com>
2020-11-08 14:34:20 +0000

    Add installer placeholder

--- lib/xjinstaller.py
+++ lib/xjinstaller.py
@@ -6,5 +6,9 @@
 
 from . import xjtoolslib
 
+# Needs to add to .zshrc/.bashrc
+# Needs to install requirements
+# python3 -m pip install -r requirements.txt
+
 if (__name__ == "__main__"):
     xjtoolslib._output_msg("Error: xjinstaller.py is a library, and can't be called directly. Try using `cd .. && python3 xjtools.py -h` for help")
