708f99cd9a84372d44708bf27eb92906873339b0
Xander Jones <xander@bugsnag.com>
2022-02-05 13:13:12 +0000

    fix pylint warnings & errors

--- tools/get_emails.py
+++ tools/get_emails.py
@@ -13,15 +13,15 @@ import os
 _VERSION = "1.0.0"
 
 def _extract_emails(input_file):
-    print("Extracting email addresses from '{0}'".format(input_file))
-    file = open(input_file, "r").read()
-    match = re.findall(r'[\w\.-]+@[\w\.-]+', file)
-    dedupe = list(set(match))
-    for index, email in enumerate(dedupe):
-        print("[{0}] {1}".format(index+1, email))
+    print("Extracting email addresses from '{input_file}'")
+    with open(input_file, "r").read() as file:
+        match = re.findall(r'[\w\.-]+@[\w\.-]+', file)
+        dedupe = list(set(match))
+        for index, email in enumerate(dedupe):
+            print("[{index+1}] {email}")
 
-if (__name__ == "__main__"):
-    p = argparse.ArgumentParser(description='tools/get_emails.py (v{0})'.format(_VERSION))
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(description='tools/get_emails.py (v{_VERSION})')
     p.add_argument("input_filepath", help="the raw input file with email addresses to extract")
     args = p.parse_args()
 
